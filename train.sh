597754189d848353da48b9e98ef25da8b57a8fb1
gp-rgb <georgia@powertp.com>
2024-01-15 13:12:19 +1000

    remove testt code

--- train.sh
+++ train.sh
@@ -27,25 +27,3 @@ echo "DATATYPE IS:  ${type}"
 echo "DATASET IS:   ${dataset}"
 echo "OPTSTRING IS: ${2} ${3}"
 
-while getopts 'abc:h' opt; do
-  case "$opt" in
-    a)
-      echo "Processing option 'a'"
-      ;;
-
-    b)
-      echo "Processing option 'b'"
-      ;;
-
-    c)
-      arg="$OPTARG"
-      echo "Processing option 'c' with '${OPTARG}' argument"
-      ;;
-   
-    ?|h)
-      echo "Usage: $(basename $0) [-a] [-b] [-c arg]"
-      exit 1
-      ;;
-  esac
-done
-shift "$(($OPTIND -1))"
